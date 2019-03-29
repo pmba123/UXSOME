137b533be0bf0703042ce40ecb469d580413c069
Lærke Høj Lange <hoej-lange@hotmail.com>
2019-03-29 11:53:04 +0100

    Opret questions.html + begyndende javascript

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,1 +1,20 @@
-getElementById('id')
+
+const questions = {
+  1: {
+    id: '1',
+    question: 'Er det nice i botanisk?',
+    option1: 'ja',
+    option2: 'måske',
+    opttion3: 'nix',
+  },
+  2: {
+    id:'2',
+    question: 'er det gode spørgsmål?',
+    option1: 'ja',
+    option2: 'måske',
+    opttion3: 'nix',
+  }
+}
+$('#sprg1').append(
+  "<div>" + questions[0].question + "</div>"
+);
